a9304f55b43a76f74db99903da5e4ec66af94766
Leandro Teixeira Wolff <leandro.teixeira.wolff@hotmail.com>
2024-05-02 15:52:43 -0300

    Add project files.

---
+++ ControleMedicamentos.ConsoleApp/Compartilhado/TelaPrincipal.cs
@@ -0,0 +1,30 @@
+﻿namespace ControleMedicamentos.ConsoleApp.Compartilhado
+{
+    internal static class TelaPrincipal
+    {
+        public static char ApresentarMenuPrincipal()
+        {
+            Console.Clear();
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("|       Controle de Medicamentos       |");
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine();
+
+            Console.WriteLine("1 - Cadastro de Pacientes");
+            Console.WriteLine("2 - Cadastro de Medicamentos");
+            Console.WriteLine("3 - Cadastro de Requisições de Saída");
+
+            Console.WriteLine("S - Sair");
+
+            Console.WriteLine();
+
+            Console.Write("Escolha uma das opções: ");
+
+            char opcaoEscolhida = Console.ReadLine()[0];
+
+            return opcaoEscolhida;
+        }
+    }
+}
